2147026447bbc29d9aa480ad0e64d8d2c9227d4f
abothunn <abothun@ucsd.edu>
2023-01-18 17:36:24 -0800

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
-    // some
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
