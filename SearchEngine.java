2e56f32be2746eeeea382d2330e94ebfc33bbdc3
abothunn <abothun@ucsd.edu>
2023-01-30 17:56:40 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,26 +1,22 @@
 import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
-    ArrayList<String> words = new ArrayList<String>();
 
     public String handleRequest(URI url) {
+        String stringVar = "";
         if (url.getPath().equals("/")) {
-            return String.format("Ashton's search engine: %d", num);
-        } else if (url.getPath().equals("/addOn")) {
-            num += 1;
-            return String.format("Word added");
+            return stringVar;
         } else {
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("history")) {
-                    words.add(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    stringVar  = stringVar + parameters[1] + "\n";
+                    return stringVar;
                 }
             }
             return "404 Not Found!";
@@ -28,7 +24,7 @@ class Handler implements URLHandler {
     }
 }
 
-class SearchEngine {
+class SearcheEngine {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
